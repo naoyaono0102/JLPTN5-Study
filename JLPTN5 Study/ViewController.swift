864f2a2fc63b7199d9931a5b846fe314059a5a6c
NAOYA ONO <naoya.ono0102@gmail.com>
2020-09-07 14:11:16 +0800

    Add print("test2")

--- JLPTN5 Study/ViewController.swift
+++ JLPTN5 Study/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("test")
+        print("test2")
         // Do any additional setup after loading the view.
     }
 
