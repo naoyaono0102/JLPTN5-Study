9b45d3d9781ccc895df555899bcd39067e0d50ce
NAOYA ONO <naoya.ono0102@gmail.com>
2020-09-07 13:56:39 +0800

    Initial Commit

---
+++ JLPTN5 Study/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JLPTN5 Study
+//
+//  Created by 尾野順哉 on 2020/09/07.
+//  Copyright © 2020 NAOYA ONO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
